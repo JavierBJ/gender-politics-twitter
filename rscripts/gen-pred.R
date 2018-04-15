0b52756d3662b2f316a5ef3442005e4686fcb05e
MrJavo94 <javierbeltranj@gmail.com>
2018-04-16 00:02:38 +0200

    R-genders updated to 13 weeks

--- rscripts/gen-pred.R
+++ rscripts/gen-pred.R
@@ -1,8 +1,14 @@
 library(gender)
 politicians <- c('dumpusers01t.csv', 'dumpusers02t.csv', 'dumpusers03t.csv', 
-                 'dumpusers04t.csv', 'dumpusers05t.csv', 'dumpusers06t.csv')
+                 'dumpusers04t.csv', 'dumpusers05t.csv', 'dumpusers06t.csv', 
+                 'dumpusers07t.csv', 'dumpusers08t.csv', 'dumpusers09t.csv', 
+                 'dumpusers10t.csv', 'dumpusers11t.csv', 'dumpusers12t.csv', 
+                 'dumpusers13t.csv')
 people <- c('dumpusers01mr.csv', 'dumpusers02mr.csv', 'dumpusers03mr.csv', 
-                 'dumpusers04mr.csv', 'dumpusers05mr.csv', 'dumpusers06mr.csv')
+                 'dumpusers04mr.csv', 'dumpusers05mr.csv', 'dumpusers06mr.csv', 
+            'dumpusers07mr.csv', 'dumpusers08mr.csv', 'dumpusers09mr.csv', 
+            'dumpusers10mr.csv', 'dumpusers11mr.csv', 'dumpusers12mr.csv', 
+            'dumpusers13mr.csv')
 names <- c('aux')
 for (f in people) {
   data <- read.csv(f, sep=';')
