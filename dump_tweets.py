34acc32dddb872d37d6d107daaac92a2bf5399b0
mrjavo94 <javierbeltranj@gmail.com>
2018-04-17 10:34:13 +0000

    Dump 14

--- dump_tweets.py
+++ dump_tweets.py
@@ -8,9 +8,9 @@ from common import metadata, text
 
 # Set these variables to the desired values before execution
 num_file = 14
-to_dump_tweets = True
-to_dump_mentions = False
-to_dump_replies = False
+to_dump_tweets = False
+to_dump_mentions = True
+to_dump_replies = True
 recover_tweets_since_id = 982616229709074433
 paths_to_accounts = {'diputados_autonomicos.csv':'twitter account', 'diputados_congreso.csv':'handle'}
 
