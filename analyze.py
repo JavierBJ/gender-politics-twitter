92863eef9531183817de0f9e08e917afe32dd699
MrJavo94 <javierbeltranj@gmail.com>
2018-07-06 14:02:20 +0200

    Scoring also must be chosen with parameters

--- analyze.py
+++ analyze.py
@@ -5,6 +5,7 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser(description='run experiments of language analysis.')
     parser.add_argument('who', choices=['author, receiver'], help='outcome to be predicted: author or receiver gender')
     parser.add_argument('alg', choices=['lasso','l1','ridge','l2','svm'], help='learning algorithm')
+    parser.add_argument('score', choices=['tr_f1', 'tr_prec', 'tr_rec', 'tr_auc', 'val_f1', 'val_prec', 'val_rec', 'val_auc'], help='scoring function to validate')
     parser.add_argument('-d', '--dbname', default='gender', help='name of the mongodb collection where the data is')
     parser.add_argument('-t', '--limit', type=int, default=0, help='maximum number of tweets recovered (0 for all)')
     parser.add_argument('-k', '--kfolds', type=int, default=10, help='k for k-fold cross-validation')
@@ -16,6 +17,7 @@ if __name__=='__main__':
     args = parser.parse_args()
     print('who:', args.who)
     print('alg:', args.alg)
+    print('score:', args.score)
     print('dbname:', args.dbname)
     print('limit:', args.limit)
     print('kfolds:', args.kfolds)
@@ -24,5 +26,5 @@ if __name__=='__main__':
     print('keepwordsrank:', args.keepwordsrank)
     print('alpha:', args.alpha)
 
-    models.analyze(args.who, args.dbname, args.limit, args.kfolds, args.stopwords, args.keepwordsfreq, args.keepwordsrank, args.alpha)
+    models.analyze(args.who, args.alg, args.score, args.dbname, args.limit, args.kfolds, args.stopwords, args.keepwordsfreq, args.keepwordsrank, args.alpha)
     # Output of this?
