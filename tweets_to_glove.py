b7ca97d96da55ee69b9abc7d915d37b153ad8eca
mrjavo94 <javierbeltranj@gmail.com>
2018-10-17 08:56:58 +0000

    Cleaned repo

--- tweets_to_glove.py
+++
@@ -1,9 +0,0 @@
-from common import text, mongo
-
-db = mongo.DB()
-tweets = db.import_tweets_mongodb()
-ws_tweets = text.tokenize(tweets)
-
-f = open('tweets_50.txt', 'w')
-f.write(ws_tweets)
-f.close()
